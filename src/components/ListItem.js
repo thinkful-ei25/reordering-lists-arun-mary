d4f0ada1de4624153a7e08d2701c323494d6cda2
Arun Seehra <seehra.a@gmail.com>
2018-11-07 11:41:31 -0600

    Test event listener

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ListItem(props) {
   return (
-    <div className="list-item">
+    <div className="list-item" draggable="true" onClick={() => console.log('CLICK!')}>
       <p>{props.name}</p>
     </div>
   );
